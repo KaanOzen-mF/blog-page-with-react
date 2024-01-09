3e1bda5b86c312ed374994c6822ff4ab4520a3c2
KaanOzen-mF <kaanozn98@gmail.com>
2024-01-09 11:38:22 +0300

    About and Contact page added navbar also added that pages | Read more for detail blog seen added

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import useFetch from "../hooks/useFetch";
 import Markdown from "react-markdown";
+import Navbar from "../components/Navbar";
+import "../Components.css";
+import { Link } from "react-router-dom";
 
 const apiToken =
   "0546198683f3f13ff19aec0987d6ee820917bfca8640ef2e68dbaa0f78b3b86a037e0f61b4bdc5b72ab614490141ec72e21e9621b26767e8b4d3e45d7bc185423ccc3c1134ee9f9189fe923c99965ba5901c89e5d392c4c42857170142ce120dbc4d33411086de41386fb8522453c5a6ee8fc654eb1dc76e1a6c6d1cef0ca3a0";
@@ -15,6 +18,7 @@ export default function Homepage() {
   if (error) return <p>Error..</p>;
   return (
     <>
+      <Navbar />
       {data.data.map((i) => (
         <div key={i.id}>
           <p>{i.attributes.blogTitle}</p>
@@ -24,6 +28,7 @@ export default function Homepage() {
             alt={`image of ${i.attributes.blogTitle}`}
             width="250px"
           />
+          <Link to={`/blog/${i.id}`}>Read More</Link>
           <hr />
         </div>
       ))}
