f63a55e8616b80d0f141a86e7825462e153ecaa3
KaanOzen-mF <kaanozn98@gmail.com>
2024-01-09 11:37:03 +0300

    Navbar and Search Bar componets added

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="navbar_container">
+        <div className="navbar_logo_container">
+          <p>Navbar</p>
+        </div>
+
+        <div className="navbar_btn_container">
+          <ul>
+            <li className="navbar_btn">
+              <Link to={"/"}>Home Page</Link>
+            </li>
+            <li className="navbar_btn">
+              <Link to={"/about"}>About</Link>
+            </li>
+            <li className="navbar_btn">
+              <Link to={"/contact"}>Contact</Link>
+            </li>
+          </ul>
+        </div>
+
+        <SearchBar onSearch={(text) => console.log(text)} />
+      </div>
+    </>
+  );
+}
