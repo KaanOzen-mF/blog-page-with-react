b9ab9d634a2041c28a3cd48db54f79d726bcba33
KaanOzen-mF <kaanozn98@gmail.com>
2024-01-09 11:38:53 +0300

    Contact and about page route added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import BlogDetail from "./pages/BlogDetail";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import "./index.css";
 
 function App() {
   return (
@@ -9,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/blog/:id" element={<BlogDetail />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
