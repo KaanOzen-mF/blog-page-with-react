24101e1e73c37d6e63d37849f3a313410cd617d4
KaanOzen-mF <kaanozn98@gmail.com>
2024-01-11 09:27:39 +0300

    Homepage categories tag functionality added

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import useFetch from "../hooks/useFetch";
 import Markdown from "markdown-to-jsx";
 import Navbar from "../components/Navbar";
@@ -16,10 +16,19 @@ export default function Homepage() {
     apiToken
   );
 
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const handleSelectCategory = (category) => {
+    setSelectedCategory(category);
+  };
+
   return (
     <div className="home_page">
       <Navbar />
-      <Categories />
+      <Categories
+        onSelectCategory={handleSelectCategory}
+        selectedCategory={selectedCategory}
+      />
       <div className="blog_main_container">
         {loading && <p>Loading...</p>}
 
@@ -29,38 +38,48 @@ export default function Homepage() {
           !error &&
           data &&
           data.data &&
-          data.data.map((i) => {
-            const date = new Date(i.attributes.publishedAt);
+          data.data
+            .filter(
+              (i) =>
+                selectedCategory === "All" ||
+                i.attributes.blogTags === selectedCategory
+            )
+            .map((i) => {
+              const date = new Date(i.attributes.publishedAt);
 
-            const options = { year: "numeric", month: "long", day: "numeric" };
-            const formattedDate = date.toLocaleDateString("en-US", options);
+              const options = {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              };
+              const formattedDate = date.toLocaleDateString("en-US", options);
 
-            const words = i.attributes.blogTexts.split(" ");
-            const previewText =
-              words.slice(0, 25).join(" ") + (words.length > 25 ? "..." : "");
+              const words = i.attributes.blogTexts.split(" ");
+              const previewText =
+                words.slice(0, 25).join(" ") + (words.length > 25 ? "..." : "");
 
-            return (
-              <div key={i.id} className="blog_card">
-                <img
-                  src={`http://localhost:1337${i.attributes.blogMedia.data[0].attributes.url}`}
-                  alt={`image of ${i.attributes.blogTitle}`}
-                  className="blog_img"
-                />
-                <p className="blog_tags">{i.attributes.blogTags}</p>
-                <div className="blog_time_data_container">
-                  <p>{formattedDate}</p>
-                  <TbPointFilled />
-                  <p>{i.attributes.minRead} min read</p>
+              return (
+                <div key={i.id} className="blog_card">
+                  <img
+                    src={`http://localhost:1337${i.attributes.blogMedia.data[0].attributes.url}`}
+                    alt={`image of ${i.attributes.blogTitle}`}
+                    className="blog_img"
+                  />
+                  <p className="blog_tags">{i.attributes.blogTags}</p>
+                  <div className="blog_time_data_container">
+                    <p>{formattedDate}</p>
+                    <TbPointFilled />
+                    <p>{i.attributes.minRead} min read</p>
+                  </div>
+                  <div className="blog_text_container">
+                    <Markdown>{previewText}</Markdown>
+                    <Link to={`/blog/${i.id}`} className="blog_text_link">
+                      Read More
+                    </Link>
+                  </div>
                 </div>
-                <div className="blog_text_container">
-                  <Markdown>{previewText}</Markdown>
-                  <Link to={`/blog/${i.id}`} className="blog_text_link">
-                    Read More
-                  </Link>
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
       </div>
       <Footer />
     </div>
